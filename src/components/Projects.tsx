205523f4351d7c8f571ca87ec8fa4824770aee65
Catalina San Juan <catalina.sanjuan@inacapmail.cl>
2024-11-18 16:29:17 -0300

    projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,13 +5,13 @@ const projects = [
   {
     title: "Sistema de Gestión de Productos",
     description: "Aplicación Web para gestionar productos y categorías. Desarrollado con Phyton y Django.",
-    image: "https://github.com/catalinasanjuan/Sistema_de_Gesti-n_de_Productos.git",
-    github: "#",
+    image: "https://blog.destacame.cl/wp-content/uploads/2024/02/Destacame_privacidad_de_datos-1024x683.jpg",
+    github: "https://github.com/catalinasanjuan/Sistema_de_Gesti-n_de_Productos.git",
     demo: "#"
   },
   {
     title: "INACAPludi",
-    description: "Aplicación Web para gestionarel préstamo de juegos de mesa del club INACAPludi para los alumnos de Inacap. Desarrollado con Next.js .",
+    description: "Aplicación Web para gestionar el préstamo de juegos de mesa del club INACAPludi para los alumnos de Inacap. Desarrollado con Next.js .",
     image: "https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?auto=format&fit=crop&q=80",
     github: "https://github.com/catalinasanjuan/INACAPludi.git",
     demo: "#"
@@ -23,7 +23,34 @@ const projects = [
     github: "https://github.com/catalinasanjuan/verduritas_sa_app.git",
     demo: "#"
   },
-  // Add more projects as needed
+  {
+    title: "Adivina el número",
+    description: "Juego web dinámico y educativo, desarrollado para mejorar la lógica y la diversión de los usuarios al intentar adivinar números generados aleatoriamente. Desarrollado en HTML, CSS y JavaScript.",
+    image: "https://i.pinimg.com/736x/d1/f5/47/d1f54779d59ff1fa53a01e8808ebc0be.jpg",
+    github: "https://github.com/catalinasanjuan/verduritas_sa_app.git",
+    demo: "#"
+  },
+  {
+    title: "Skymon Dash",
+    description: "Juego de plataformas desarrollado con Python y Pygame.",
+    image: "https://i.pinimg.com/736x/1f/54/c8/1f54c85d64378fca64d2fc075ba042c3.jpg",
+    github: "https://github.com/catalinasanjuan/verduritas_sa_app.git",
+    demo: "#"
+  },
+  {
+    title: "Calculadora de notas",
+    description: "Herramienta Web práctica y eficiente para estudiantes y profesores que deseen calcular promedios académicos de manera rápida y sencilla. Desarrollado en HTML, CSS y JavaScript",
+    image: "https://i.pinimg.com/736x/b1/51/3d/b1513d00bce64b94d19441454385e3e3.jpg",
+    github: "https://github.com/catalinasanjuan/verduritas_sa_app.git",
+    demo: "#"
+  },
+  {
+    title: "INACAPludi MarketPlace - Landing Page",
+    description: "solución innovadora para el comercio en línea, diseñada para ofrecer una experiencia de usuario sencilla y eficiente",
+    image: "https://www.dimensiona.com/wp-content/uploads/2021/01/beneficios-app-768x439.jpg",
+    github: "https://github.com/catalinasanjuan/verduritas_sa_app.git",
+    demo: "#"
+  },
 ];
 
 export default function Projects() {
